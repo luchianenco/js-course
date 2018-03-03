a22191cc400c51fa3c923da7bd980cd4e83994a5
Serghei Luchianenco <s@luchianenco.com>
2018-03-03 15:58:21 +0100

    * fixed object.create polyfill * added properties object argument

--- src/lesson1/index1-2.js
+++ src/lesson1/index1-2.js
@@ -1,12 +1,13 @@
-const Object = {
-    create: function(proto) {
-        if (typeof proto !== 'object' && typeof proto !== 'function' ) {
-            throw new Error('Object prototype may be only type of object, given type of ' + typeof proto);
-        }
-
-        function Fn() {};
-        Fn.prototype = proto;
-        
-        return new Fn()
+Object.create = function(proto, propertiesObject) {
+    if (typeof proto !== 'object' && typeof proto !== 'function' ) {
+        throw new Error('Object prototype may be only type of object, given type of ' + typeof proto);
     }
-}
+
+    if (typeof propertiesObject !== 'object' && typeof propertiesObject !== 'function' ) {
+        throw new Error('Object Properties may be only type of object, given type of ' + typeof proto);
+    }
+
+    propertiesObject.__proto__ = proto;
+
+    return propertiesObject;
+};
