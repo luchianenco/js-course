e25baf7d73fa0f3e46b94024691047e3c9f64391
Serghei Luchianenco <s@luchianenco.com>
2018-03-14 14:14:38 +0100

    * optimized intersect func

--- src/lesson3.3/index.js
+++ src/lesson3.3/index.js
@@ -9,12 +9,10 @@ export function intersect(node1, node2) {
 
     const e1 = node1.getBoundingClientRect();
     const e2 = node2.getBoundingClientRect();
-    const result = !(
+    return !(
         e1.right < e2.left ||
         e1.left > e2.right ||
         e1.bottom < e2.top ||
         e1.top > e2.bottom
     );
-
-    return result
 }
