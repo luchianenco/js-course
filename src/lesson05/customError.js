09a980fc6aa16c0229e81ac2fdd8ad54105552e2
Serghei Luchianenco <s@luchianenco.com>
2018-03-18 23:26:59 +0100

    * removed lesson5 from master

--- src/lesson05/customError.js
+++
@@ -1,9 +0,0 @@
-class LogicError extends Error {
-    constructor(...args) {
-        super(...args);
-        Error.captureStackTrace(this, LogicError);
-    }
-}
-
-console.log('testing custom error');
-throw new LogicError('Logic Exception');
