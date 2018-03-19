6062ae2338b8a43ac3e500c03c3c0b69564217fc
Serghei Luchianenco <s@luchianenco.com>
2018-03-19 17:45:03 +0100

    * added custom error stack output

--- src/lesson05/customError.js
+++ src/lesson05/customError.js
@@ -5,5 +5,10 @@ class LogicError extends Error {
     }
 }
 
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+    console.log(err.stack);
+});
+
 console.log('testing custom error');
 throw new LogicError('Logic Exception');
